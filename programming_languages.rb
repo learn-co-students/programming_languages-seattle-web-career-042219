965f2c57c92f43f9bbf082833b0fa3f0f261b17e
github username <github email address>
2019-04-15 14:34:10 -0700

    lang interp

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,46 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages[:oo].merge(languages[:functional]).each do |lang, info|
+  	new_hash[lang] = info
+  	new_hash[lang][:style] = []
+  	if languages[:oo].keys.include?(lang)
+  		new_hash[lang][:style].push(:oo)
+  	end
+  	if languages[:functional].keys.include?(lang)
+  		new_hash[lang][:style].push(:functional)
+  	end
+  end
 end
