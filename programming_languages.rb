64206a5708a1525ef2d0449a8c951316abb1b800
Sean Watters <sean@h2os.org>
2019-04-18 05:36:03 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,50 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+
+
 def reformat_languages(languages)
   # your code here
+  new_language_format = {}
+
+  languages.each {|style, language|
+    language.each {|language, type|
+      if new_language_format.has_key?(language)
+        new_language_format[language][:style] << style
+      else
+        new_language_format[language] = type
+        new_language_format[language][:style] = [style]
+      end
+    }
+  }
+ new_language_format
 end
