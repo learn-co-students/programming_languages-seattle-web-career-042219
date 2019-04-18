8bc44dc25c1be3572d8fffefb7d9df591c47fc14
Caleb Watters <caleb@h2os.org>
2019-04-18 18:00:38 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each{|style, list_of_langs|
+    list_of_langs.each{ |name, info|
+      if new_hash.has_key?(name)
+        new_hash[name][:style] << style
+      else 
+        new_hash[name] = {:style => [style], :type => info[:type]}
+      end
+    }
+  }
+  return new_hash
 end
